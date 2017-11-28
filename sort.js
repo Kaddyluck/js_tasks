68bbdf1e8ab59c822013d09a0b65c7a31ffe2d58
Kaddyluck <vvberdinskiy@yandex.ru>
2017-11-28 14:16:40 +0300

    Add sorting

--- sort.js
+++ sort.js
@@ -1,3 +1,16 @@
-console.log("Hello");
-var items = [4, 2, 6, 5, 3, 9];
-console.log(items);
+var items = [10, 3, 12, 4, 1, 7];
+
+function QuickSort(array)
+{
+    if (array.length == 0) return [];
+    var a = [], b = [], p = array[0];
+    for (var i = 1; i < array.length; i++) {
+      if (array[i] < p)
+        a[a.length] = array[i];
+      else
+        b[b.length] = array[ i ];
+     }
+    return QuickSort(a).concat(p, QuickSort(b));
+}
+
+console.log(QuickSort(items));
